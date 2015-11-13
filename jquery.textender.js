bf75df98896f38aa82e118d33474532633870f30
Olivier Cauwenbergh <oliviercauwenbergh@telenet.be>
2015-11-13 08:30:35 +0100

    Changed jQuery plugin Bootstrap style

--- jquery.textender.js
+++ jquery.textender.js
@@ -1,64 +1,72 @@
-(function( $ ) {
++function ($) {
+  'use strict';
 
-  $.fn.textender = function( options ) {
+  var Textender = function (textarea, options) {
+    this.$textarea  = $(textarea)
+    this.$twin      = this.maketwin(this.$textarea)
+    this.options    = $.extend({}, Textender.DEFAULTS, options)
 
-    return this.each(function() {
+    this.init()
+  }
 
-      var $textarea,
-          $twin,
-          opts = $.extend({}, $.fn.textender.defaults, options );
+  Textender.VERSION = '1.1.0'
 
-      // Making a invisible div with the same css values as the textarea.
-      // Values that affect the height of the div.
-      var maketwin = function( textarea ) {
-        return $('<div class="textarea-twin"></div>').css({
-          display: 'none',
-          fontFamily: textarea.css('fontFamily'),
-          fontSize: textarea.css('fontSize'),
-          lineHeight: textarea.css('lineHeight'),
-          padding: textarea.css('padding'),
-          whiteSpace: textarea.css('whiteSpace'),
-          width: textarea.css('width'),
-          wordWrap: textarea.css('wordWrap')
-        }).insertAfter( textarea );
-      };
-
-      $textarea = $(this);              // Current textarea
-      $twin = maketwin( $textarea );    // Textarea's twin
-
-      // Initialize the textarea with its height values.
-      // This is necessary to avoid little jump in height when start typing
-      // and to always reserve the min-height of the textarea defined by the user.
-      $textarea.css({
-        padding: $textarea.css('padding'),
-        margin: $textarea.css('margin'),
-        height: $textarea.height(),
-        minHeight: $textarea.height(),
-      });
-
-      // On 'keyup' and 'paste' the textarea input value is being inserted into the twin.
-      // Then the height of the textarea changes based on the twin's height
-      $textarea.on('keyup paste', function() {
-        var text = $textarea.val();                   // Value of the textarea
-        var html = $('<div/>').text( text ).html();   // Value encoded into html
-        $twin.html( html );
-
-        if (  $twin.height() > $textarea.height() &&
-              $twin.height() <= opts.maxrows * parseFloat( $textarea.css('lineHeight') ) &&
-              $twin.height() >= parseFloat( $textarea.css('minHeight') ) )
-          $textarea.height( $twin.height() + parseFloat( $textarea.css('paddingBottom') ) );
-
-        if ( $twin.height() <= parseFloat( $textarea.css('minHeight') ) )
-          $textarea.height( $textarea.css('minHeight') );
-      });
-
-    });
-
-  };
-
-  // Textender defaults
-  $.fn.textender.defaults = {
+  Textender.DEFAULTS = {
     maxrows: 10
-  };
+  }
 
-})( jQuery );
+  Textender.prototype.init = function () {
+    var t = this;
+    t.$textarea.css({
+      padding: t.$textarea.css('padding'),
+      margin: t.$textarea.css('margin'),
+      height: t.$textarea.height(),
+      minHeight: t.$textarea.height(),
+    })
+
+    t.$textarea.on('keyup paste', function() {
+      var text = t.$textarea.val()
+      var html = $('<div/>').text( text ).html()
+      t.$twin.html( html )
+
+      if (  t.$twin.height() > t.$textarea.height() &&
+            t.$twin.height() <= t.options.maxrows * parseFloat( t.$textarea.css('lineHeight') ) &&
+            t.$twin.height() >= parseFloat( t.$textarea.css('minHeight') ) )
+        t.$textarea.height( t.$twin.height() + parseFloat( t.$textarea.css('paddingBottom') ) )
+
+      if ( t.$twin.height() <= parseFloat( t.$textarea.css('minHeight') ) )
+        t.$textarea.height( t.$textarea.css('minHeight') )
+    })
+  }
+
+  Textender.prototype.maketwin = function (textarea) {
+    return $('<div class="textarea-twin"></div>').css({
+      display: 'none',
+      fontFamily: textarea.css('fontFamily'),
+      fontSize: textarea.css('fontSize'),
+      lineHeight: textarea.css('lineHeight'),
+      padding: textarea.css('padding'),
+      whiteSpace: textarea.css('whiteSpace'),
+      width: textarea.css('width'),
+      wordWrap: textarea.css('wordWrap')
+    }).insertAfter( textarea );
+  }
+
+  function Plugin(option) {
+    return this.each(function () {
+      var options = typeof option == 'object' && option
+      var textender = new Textender(this, options)
+    })
+  }
+
+  var old = $.fn.textender
+
+  $.fn.textender              = Plugin
+  $.fn.textender.Constructor  = Textender
+
+  $.fn.textender.noConflict = function () {
+    $.fn.textender = old
+    return this
+  }
+
+}(jQuery);
